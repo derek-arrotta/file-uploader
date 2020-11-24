4f69f3e625468bbdee863d288a65a7df767ddbb1
Derek Arrotta <derek.arrotta@gmail.com>
2020-11-24 11:02:45 -0500

    add contructor to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import SearchBar from './SearchBar/SearchBar';
 import FilterableList from './FilterableList/FilterableList';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
+
   render() {
     return (
       <div className="App">
